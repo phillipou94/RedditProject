95e7fd11bfd7c5f5714353fddd4d1b1c0816cc13
phillipou94 <phillipou@Phillips-MacBook-Pro-2.local>
2014-12-14 16:01:39 -0800

    added css and persistent log in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212225545) do
+ActiveRecord::Schema.define(version: 20141214230626) do
 
   create_table "posts", force: true do |t|
     t.text     "body"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20141212225545) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
