6ad3bbc43fe9af5b858a825f35aa47e80679592f
Andrew Brandt <abrandt@makeschool.com>
2014-12-14 17:31:01 -0800

    new post - redirect to root if not logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def new
-  	@post = Post.new 
+    redirect_to root_path if !logged_in?
+  	@post = Post.new
   end
 
   def create
@@ -9,7 +10,7 @@ class PostsController < ApplicationController
     @post.user_id=1
     if(@post.save)
       redirect_to root_path
-    else 
+    else
       render new
     end
   end
